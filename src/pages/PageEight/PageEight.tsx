6bcfe4d924e1a796b6558d62ed8f872051652f19
priscilla-02 <136003704+priscilla-02@users.noreply.github.com>
2024-02-28 11:12:16 +0000

    WIP IVA question

--- src/pages/PageEight/PageEight.tsx
+++ src/pages/PageEight/PageEight.tsx
@@ -1,10 +1,33 @@
-import { Header } from '../../styles/globalStyles'
+import { useSelector } from 'react-redux'
+import { RootState } from '../../store/index'
+import { useNavigate } from 'react-router-dom'
+import { Header, Title } from '../../styles/globalStyles'
+import { Button, Container } from './PageEightStyle'
 
 function PageEight() {
+    const navigate = useNavigate()
+    const firstNameState = useSelector(
+        (state: RootState) => state.userInfo.userInfo.firstName
+    )
+
+    const handleSumbit = () => {
+        navigate('/page/complete')
+    }
+
     return (
-        <>
-            <Header>Page 8</Header>
-        </>
+        <Container>
+            <Header>
+                We just need a couple more things from you, {firstNameState}...
+            </Header>
+            <Title>
+                Have you ever been in or are proposed to be in an IVA or
+                declared bankrupt?*
+            </Title>
+            <Button colour={true} onClick={() => handleSumbit()}>
+                Yes
+            </Button>
+            <Button colour={false}>No</Button>
+        </Container>
     )
 }
 
