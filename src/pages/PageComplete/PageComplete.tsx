ba2be32c168b6b94ca4501d35ce98ac643cc8612
priscilla-02 <136003704+priscilla-02@users.noreply.github.com>
2024-02-28 11:13:05 +0000

    edit PageComplete

--- src/pages/PageComplete/PageComplete.tsx
+++ src/pages/PageComplete/PageComplete.tsx
@@ -1,7 +1,34 @@
-import { Header } from '../../styles/globalStyles'
+import { useSelector } from 'react-redux'
+import { RootState } from '../../store/index'
+import { useNavigate } from 'react-router-dom'
+import { Container, MessageBox, Message } from './PageCompleteStyle'
+import { Title } from '../../styles/globalStyles'
 
 function PageComplete() {
-    return <Header>Page Complete</Header>
+    const navigate = useNavigate()
+    const firstNameState = useSelector(
+        (state: RootState) => state.userInfo.userInfo.firstName
+    )
+
+    return (
+        <Container>
+            <Title>
+                Thank you, {firstNameState}! Your extra answers have been
+                submitted.
+            </Title>
+            <MessageBox>
+                <Message>
+                    These will allow our expert team to begin working on your
+                    case right away, and we look to get back to you as soon as
+                    possible.
+                </Message>
+                <Title>
+                    Don't forget to keep an eye out for a call or email from a
+                    member of our team concerning your claim.
+                </Title>
+            </MessageBox>
+        </Container>
+    )
 }
 
 export default PageComplete
