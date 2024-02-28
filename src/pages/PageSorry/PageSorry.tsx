b4e7e9035576d856ed895e15e68013a0c57236c3
priscilla-02 <136003704+priscilla-02@users.noreply.github.com>
2024-02-28 11:13:26 +0000

    add start over feat

--- src/pages/PageSorry/PageSorry.tsx
+++ src/pages/PageSorry/PageSorry.tsx
@@ -1,7 +1,29 @@
-import { Header } from '../../styles/globalStyles'
+import { useNavigate } from 'react-router-dom'
+import { ContinueButton, Title } from '../../styles/globalStyles'
+import { Container, Message, MessageBox } from './PageSorryStyle'
 
 function PageSorry() {
-    return <Header>Page Sorry</Header>
+    const navigate = useNavigate()
+
+    const handleClick = () => {
+        navigate('/')
+    }
+
+    return (
+        <Container>
+            <Title>We’re sorry about that!</Title>
+            <MessageBox>
+                <Message>
+                    Based on your given answers, it looks like you won’t qualify
+                    to claim for compensation.
+                </Message>
+                <Message>
+                    If you believe this was a mistake, please try again below.
+                </Message>
+            </MessageBox>
+            <ContinueButton onClick={handleClick}>Start Over</ContinueButton>
+        </Container>
+    )
 }
 
 export default PageSorry
---
+++ src/pages/PageSorry/PageSorryStyle.tsx
@@ -0,0 +1,23 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`
+
+export const MessageBox = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 8px;
+    margin: 32px 0;
+`
+export const Message = styled.div`
+    font-family: Nunito Sans;
+    font-size: 14px;
+    font-weight: 400;
+    line-height: 19.1px;
+    letter-spacing: 0em;
+    text-align: center;
+    color: #172c3b;
+`
