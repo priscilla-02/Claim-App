c24a2ca45b250f3a9316b7c5e8e212848b393ca2
priscilla-02 <136003704+priscilla-02@users.noreply.github.com>
2024-02-28 11:11:51 +0000

    WIP submit claim

--- src/pages/PageSeven/PageSeven.tsx
+++ src/pages/PageSeven/PageSeven.tsx
@@ -1,14 +1,51 @@
-import { Link } from 'react-router-dom'
-import { ContinueButton, Header } from '../../styles/globalStyles'
+import { useSelector } from 'react-redux'
+import { RootState } from '../../store/index'
+import { useNavigate } from 'react-router-dom'
+
+import { ContinueButton, Header, Title } from '../../styles/globalStyles'
+import {
+    Container,
+    DisclaimerText,
+    DisclaimerTextSpan,
+    ReviewText,
+    ReviewTextSpan,
+    SignatureContainer,
+} from './PageSevenStyle'
 
 function PageSeven() {
+    const navigate = useNavigate()
+    const firstNameState = useSelector(
+        (state: RootState) => state.userInfo.userInfo.firstName
+    )
+
+    const handleClaimSumbit = () => {
+        navigate('/page/8')
+    }
+
     return (
-        <>
-            <Header>Page 7</Header>
-            <Link to="/page/8">
-                <ContinueButton>Continue</ContinueButton>
-            </Link>
-        </>
+        <Container>
+            <Header>
+                Copy You’re almost finished, {firstNameState}. You just need to
+                review this…
+            </Header>
+            <Title>Please sign here to submit your Plevin claim.</Title>
+            <SignatureContainer>
+                <ReviewText>
+                    Review your <ReviewTextSpan>claim pack here</ReviewTextSpan>
+                    .
+                </ReviewText>
+            </SignatureContainer>
+            <DisclaimerText>
+                <DisclaimerTextSpan>Disclaimer:</DisclaimerTextSpan> By
+                proceeding, you agree to be the terms listed in Privacy Policy.
+                Additionally, you agree to be contacted by Veriform Limited,
+                OPOP Services Limited, or any of our affiliated partners in
+                regards to your claim and future opportunities.
+            </DisclaimerText>
+            <ContinueButton onClick={() => handleClaimSumbit()}>
+                Submit Claim
+            </ContinueButton>
+        </Container>
     )
 }
 
---
+++ src/pages/PageSeven/PagesevenStyle.tsx
@@ -0,0 +1,60 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+`
+
+export const SignatureContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+`
+
+export const ReviewText = styled.h1`
+    font-family: Nunito Sans;
+    font-size: 14px;
+    font-weight: 400;
+    line-height: 19px;
+    letter-spacing: 0em;
+    text-align: center;
+    color: #254358;
+`
+export const ReviewTextSpan = styled.span`
+    font-family: Nunito Sans;
+    font-size: 14px;
+    font-weight: 700;
+    line-height: 19px;
+    letter-spacing: 0em;
+    text-align: center;
+    color: #21d63e;
+    text-decoration: underline;
+`
+export const DisclaimerText = styled.h1`
+    font-family: Nunito Sans;
+    font-size: 12px;
+    font-weight: 400;
+    line-height: 16.37px;
+    letter-spacing: 0em;
+    text-align: center;
+    color: #254358;
+    background-color: #f6f6f6;
+    width: 350px;
+    height: 96px;
+    padding: 8px;
+    border-radius: 10px;
+    gap: 8px;
+`
+
+export const DisclaimerTextSpan = styled.span`
+    font-family: Nunito Sans;
+    font-size: 12px;
+    font-weight: 700;
+    line-height: 16.37px;
+    letter-spacing: 0em;
+    text-align: center;
+    color: #254358;
+`
